5f13695c552285023b55c5f94f65ad94a98a2831
Jeewon Heo <jeewon@jeewons-mbp.kpn>
2023-05-11 20:18:44 +0200

    .

---
+++ docker/db/init.sql
@@ -0,0 +1,14 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR (50) UNIQUE NOT NULL,
+    password VARCHAR NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS urls (
+    id SERIAL PRIMARY KEY,
+    original VARCHAR NOT NULL,
+    short VARCHAR (50) UNIQUE NOT NULL,
+    username VARCHAR (50) NOT NULL,
+    FOREIGN KEY (username)
+        REFERENCES users(username)
+);
